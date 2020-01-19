375f02ebd5c459cc614ca12525df0f3718e974d5
Edenilton <edeniltonborba.adm@gmail.com>
2020-01-19 21:17:30 +0100

    schema

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+DROP TABLE IF EXISTS burgers;
+
+CREATE TABLE burgers
+(
+id INT PRIMARY KEY AUTO_INCREMENT,
+burger_name VARCHAR(30),
+devoured BOOLEAN DEFAULT false,
+);
