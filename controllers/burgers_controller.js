907dcec2aad1d868dfd20f7255ebba18e5a18835
Edenilton <edeniltonborba.adm@gmail.com>
2020-01-20 18:46:41 +0100

    burgers_controller.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+  
+  router.post("/api/burgers", function(req, res) {
+    burger.insertOne([req.body.name], function(result) {
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/api/burgers/:id", function(req, res) {
+    burger.updateOne([req.params.id], function(result) {
+      if (result.changedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
+  module.exports = router;
